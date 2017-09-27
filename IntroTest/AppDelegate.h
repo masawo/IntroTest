ea4b4cc68157a2c0965ac2691fb2f77f74fe12db
masawo <masawo@gmail.com>
2017-09-27 13:12:54 +0900

    Initial Commit

---
+++ IntroTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroTest
+//
+//  Created by Masao S on 2017/09/27.
+//  Copyright © 2017年 miil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntroTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntroTest
+//
+//  Created by Masao S on 2017/09/27.
+//  Copyright © 2017年 miil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
